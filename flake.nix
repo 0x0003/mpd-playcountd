daa8ab1cfadd6549db4457c3569b6153cd25e616
0x0003 <0x0003@users.noreply.github.com>
2026-07-31 01:07:20 +0900

    flake: bump to 0.1.1

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
     in {
       default = pkgs.buildGoModule {
         pname = "mpd-playcountd";
-        version = "0.1.0";
+        version = "0.1.1";
         src = ./.;
         vendorHash = "sha256-pbA/AlBz3cQYRTMnQ/qBPcinYOKokrBLNhkbRTq54gE=";
       };
